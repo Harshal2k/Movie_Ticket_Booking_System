a4619870b542e4dc0bbb4ee15486b6f503f4530e
Harshal Gosawi <harshalgosawi@gmail.com>
2022-12-28 15:56:35 +0530

    Added boilerplate code and functions

--- ticketBookingSystem.c
+++ ticketBookingSystem.c
@@ -1,8 +1,129 @@
 #include <stdio.h>
 
+struct Movie
+{
+    char name[30];
+    int durationMin;
+};
+
+struct Screen
+{
+    int screenNum;
+    struct Movie *movie;
+    int seats[15][15];
+    char dateTime[20];
+    struct Screen *next;
+};
+
+void admin()
+{
+}
+
+void user()
+{
+}
+
+void setMovie()
+{
+}
+
+void removeMovie()
+{
+}
+
+void displaySeats()
+{
+}
+
+void displayMovies()
+{
+}
+
+void bookTicket()
+{
+}
+
+void cancelTicket()
+{
+}
+
+void showsAvailable()
+{
+}
+
+void bookedShows()
+{
+}
+
+void extraFunction1()
+{
+}
+
+void extraFunction2()
+{
+}
+
+void extraFunction3()
+{
+}
+
+void extraFunction4()
+{
+}
+
+void extraFunction5()
+{
+}
+
+void extraFunction6()
+{
+}
+
+void extraFunction7()
+{
+}
+
+void extraFunction8()
+{
+}
+
+void extraFunction9()
+{
+}
+
+void extraFunction10()
+{
+}
+
 int main()
 {
+    int option;
     printf("\nMOVIE TICKET BOOKING SYSTEM");
+    while (1)
+    {
+        printf("\n1. Admin");
+        printf("\n2. User");
+        printf("\n3. Exit");
+        printf("\nEnter your option: ");
+        scanf("%d", &option);
+        switch (option)
+        {
+        case 1:
+            printf("\n In Admin");
+            admin();
+            break;
+        case 2:
+            printf("\n In User");
+            user();
+            break;
+        case 3:
+            exit(0);
+            break;
+        default:
+            printf("\nInvalid Option :(");
+            break;
+        }
+    }
 
     return 0;
 }
