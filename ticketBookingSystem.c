3d53ce5a02c066741939743962b67ce4e7f39171
Harshal Gosawi <harshalgosawi@gmail.com>
2022-12-28 20:58:06 +0530

    Added code to addMovies

--- ticketBookingSystem.c
+++ ticketBookingSystem.c
@@ -8,7 +8,8 @@ struct Movie
     int screen1;
     int screen2;
     int screen3;
-};
+    struct Movie *next;
+} *movieHead = NULL;
 
 struct Screen
 {
@@ -19,11 +20,20 @@ struct Screen
     int day;
     int month;
     int year;
+    char time;
     struct Screen *next;
 } *screen1, *screen2, *screen3;
 
-void extraFunction2()
+void showMovies(struct Movie **head)
 {
+    printf("\n-------MOVIES LIST---------------");
+    struct Movie *temp;
+    temp = *head;
+    while (temp != NULL)
+    {
+        printf("\nMovie = %s", temp->name);
+        temp = temp->next;
+    }
 }
 
 void extraFunction3()
@@ -58,7 +68,7 @@ void extraFunction10()
 {
 }
 
-void isDateValid(int dd, int mm, int yy)
+int isDateValid(int dd, int mm, int yy)
 {
     // check year
     if (yy >= 1900 && yy <= 9999)
@@ -68,31 +78,58 @@ void isDateValid(int dd, int mm, int yy)
         {
             // check days
             if ((dd >= 1 && dd <= 31) && (mm == 1 || mm == 3 || mm == 5 || mm == 7 || mm == 8 || mm == 10 || mm == 12))
-                printf("Date is valid.\n");
-            else if ((dd >= 1 && dd <= 30) && (mm == 4 || mm == 6 || mm == 9 || mm == 11))
-                printf("Date is valid.\n");
-            else if ((dd >= 1 && dd <= 28) && (mm == 2))
-                printf("Date is valid.\n");
-            else if (dd == 29 && mm == 2 && (yy % 400 == 0 || (yy % 4 == 0 && yy % 100 != 0)))
-                printf("Date is valid.\n");
-            else
-                printf("Day is invalid.\n");
+                return 1 else if ((dd >= 1 && dd <= 30) && (mm == 4 || mm == 6 || mm == 9 || mm == 11)) return 1 else if ((dd >= 1 && dd <= 28) && (mm == 2)) return 1 else if (dd == 29 && mm == 2 && (yy % 400 == 0 || (yy % 4 == 0 && yy % 100 != 0))) return 1 else printf("Day is invalid.\n");
+            return 0;
         }
         else
         {
             printf("Month is not valid.\n");
+            return 0;
         }
     }
     else
     {
         printf("Year is not valid.\n");
+        return 0;
     }
-
-    return;
 }
 
-void setMovie()
+void setMovie(struct Movie **head)
 {
+    char movieName[30];
+    int dd, mm, yy, duration;
+    printf("\nEnter Movie Name: ");
+    scanf(" ");
+    fgets(movieName, 30, stdin);
+    printf("\nEnter Movie Duration (in minutes): ");
+    scanf("%d", &duration);
+    printf("\nEnter Release date (in format dd/mm/yyyy): ");
+    scanf("%d/%d/%d", &dd, &mm, &yy);
+    if (isDateValid(dd, mm, yy) == 0)
+    {
+        return;
+    }
+    struct Movie *newMovie = (struct Movie *)malloc(sizeof(struct Movie));
+    newMovie->screen1 = 0;
+    newMovie->screen2 = 0;
+    newMovie->screen3 = 0;
+    newMovie->next = NULL;
+    strcpy(newMovie->name, movieName);
+    newMovie->durationMin = duration;
+    struct Movie *temp;
+    temp = *head;
+    if (temp == NULL)
+    {
+        *head = newMovie;
+    }
+    else
+    {
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = newMovie;
+    }
 }
 
 void removeMovie()
@@ -173,36 +210,42 @@ void admin()
     char password[20];
     char movieName[30];
     int dd, mm, yy;
+    struct Movie *temp = movieHead;
     printf("\nEnter Admin Password:");
     scanf(" ");
     fgets(password, 20, stdin);
-    if (0 != strcmp(password, "harshal2k"))
+    if (0 != strcmp(password, "harshal2k\n"))
     {
         printf("\nInvalid Password");
         return;
     }
     int option;
-    printf("\n****WELCOME ADMIN****");
-    printf("\n1. Add Movie");
-    printf("\n2. Remove Movie");
-    printf("\n3. Remove Show");
-    printf("\n4. Exit");
-    switch (option)
+    while (1)
     {
-    case 1:
-        printf("\nEnter Movie Name: ");
-        scanf(" ");
-        fgets(movieName, 30, stdin);
-        printf("Enter date (DD/MM/YYYY format): ");
-        scanf("%d/%d/%d", &dd, &mm, &yy);
-        setMovie();
-        break;
-    case 2:
-        removeMovie();
-        break;
-
-    default:
-        break;
+        printf("\n****WELCOME ADMIN****");
+        printf("\n1. Add Movie");
+        printf("\n2. Remove Movie");
+        printf("\n3. Remove Show");
+        printf("\n4. Exit");
+        printf("\nEnter your option: ");
+        scanf("%d", &option);
+        switch (option)
+        {
+        case 1:
+            setMovie(&movieHead);
+            break;
+        case 2:
+            removeMovie();
+            break;
+        case 3:
+            showMovies(&movieHead);
+            // remove show()
+            break;
+        case 4:
+            return;
+        default:
+            break;
+        }
     }
 }
 
