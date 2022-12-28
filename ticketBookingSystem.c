019cb07d4043bd9f31e5695da18ed7eb2c1309fd
Harshal Gosawi <harshalgosawi@gmail.com>
2022-12-29 01:54:33 +0530

    Completed AddMovie, setScreen and removeMovie functionality

--- ticketBookingSystem.c
+++ ticketBookingSystem.c
@@ -20,9 +20,10 @@ struct Screen
     int day;
     int month;
     int year;
-    char time;
+    char time[20];
+    int seatsCount;
     struct Screen *next;
-} *screen1=NULL, *screen2, *screen3;
+} *screen1 = NULL, *screen2 = NULL, *screen3 = NULL;
 
 void showMovies(struct Movie **head)
 {
@@ -36,12 +37,63 @@ void showMovies(struct Movie **head)
     }
 }
 
-void extraFunction3()
+void setScreen(struct Screen **screenHead, int screenNum, int showId, struct Movie **movie)
 {
+    printf("\n====== IN SETSCREEN %d", showId);
+    struct Screen *newShow = (struct Screen *)malloc(sizeof(struct Screen));
+    newShow->screenNum = screenNum;
+    newShow->showID = showId;
+    newShow->movie = *movie;
+    newShow->next = NULL;
+    struct Screen *temp;
+    temp = *screenHead;
+    if (temp == NULL)
+    {
+        *screenHead = newShow;
+    }
+    else
+    {
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = newShow;
+    }
 }
 
-void extraFunction4()
+void displayShows(struct Screen **sHead1, struct Screen **sHead2, struct Screen **sHead3)
 {
+    printf("\n-------SHOW LIST 1---------------");
+    struct Screen *temp;
+    temp = *sHead1;
+    for (int i = 0; i < 3; i++)
+    {
+        if (i == 0)
+        {
+            printf("\n-------SHOW LIST 1---------------");
+            temp = *sHead1;
+        }
+        else if (i == 1)
+        {
+            printf("\n-------SHOW LIST 2---------------");
+            temp = *sHead2;
+        }
+        else
+        {
+            printf("\n-------SHOW LIST 3---------------");
+            temp = *sHead3;
+        }
+        while (temp != NULL)
+        {
+            printf("\n-----------------------------------");
+            printf("\nMovie: %s", temp->movie->name);
+            printf("\nDuration: %d", temp->movie->durationMin);
+            printf("\nScreenNum: %d", temp->screenNum);
+            printf("\nShowId: %d", temp->showID);
+            printf("\n-----------------------------------");
+            temp = temp->next;
+        }
+    }
 }
 
 void extraFunction5()
@@ -102,21 +154,35 @@ int isDateValid(int dd, int mm, int yy)
     }
 }
 
-void setMovie(struct Movie **head)
+void setMovie(struct Movie **head, struct Screen **sHead1, struct Screen **sHead2, struct Screen **sHead3)
 {
     char movieName[30];
-    int dd, mm, yy, duration;
+    int dd, mm, yy, duration, screens = 0, valid = 0, scre[3];
     printf("\nEnter Movie Name: ");
     scanf(" ");
     fgets(movieName, 30, stdin);
+    movieName[strcspn(movieName, "\n")] = 0;
     printf("\nEnter Movie Duration (in minutes): ");
     scanf("%d", &duration);
-    printf("\nEnter Release date (in format dd/mm/yyyy): ");
-    scanf("%d/%d/%d", &dd, &mm, &yy);
-    if (isDateValid(dd, mm, yy) == 0)
-    {
-        return;
-    }
+    // printf("\nEnter Release date (in format dd/mm/yyyy): ");
+    // scanf("%d/%d/%d", &dd, &mm, &yy);
+    // if (isDateValid(dd, mm, yy) == 0)
+    // {
+    //     return;
+    // }
+    // printf("\nEnter total screens (MAX 3): ");
+    // scanf("%d", &screens);
+    // if (screens > 3)
+    // {
+    //     printf("\nOnly 3 screens are avialable");
+    //     return;
+    // }
+    // else if (screens <= 0)
+    // {
+    //     printf("\nScreens cannot be less then or equal to Zero");
+    //     return;
+    // }
+
     struct Movie *newMovie = (struct Movie *)malloc(sizeof(struct Movie));
     newMovie->screen1 = 0;
     newMovie->screen2 = 0;
@@ -124,6 +190,57 @@ void setMovie(struct Movie **head)
     newMovie->next = NULL;
     strcpy(newMovie->name, movieName);
     newMovie->durationMin = duration;
+    for (int i = 0; i < 3; i++)
+    {
+        char romanNum[3];
+        if (i == 0 && screen1 == NULL)
+        {
+            strcpy(romanNum, "I");
+        }
+        else if (i == 1 && screen2 == NULL)
+        {
+            strcpy(romanNum, "II");
+        }
+        else if (i == 2 && screen3 == NULL)
+        {
+            strcpy(romanNum, "III");
+        }
+        else
+        {
+            continue;
+        }
+        char option;
+        printf("\nDo you want to play this movie on Screen %s", romanNum);
+        printf("\nPress Y for yes OR any other character as NO");
+        scanf(" %c", &option);
+        if (option == 'y' || option == 'Y')
+        {
+            int shows = (960 / duration) + 1;
+            int count;
+            for (int j = 0; j < shows; j++)
+            {
+                if (i == 0)
+                {
+                    setScreen(&screen1, 1, (j + 1), &newMovie);
+                    newMovie->screen1 = 1;
+                }
+                else if (i == 1)
+                {
+                    setScreen(&screen2, 2, (j + 1), &newMovie);
+                    newMovie->screen2 = 1;
+                }
+                else
+                {
+                    setScreen(&screen3, 3, (j + 1), &newMovie);
+                    newMovie->screen3 = 1;
+                }
+            }
+        }
+        else
+        {
+            printf("\nOHHH NOOOOOOOOOO");
+        }
+    }
     struct Movie *temp;
     temp = *head;
     if (temp == NULL)
@@ -140,8 +257,60 @@ void setMovie(struct Movie **head)
     }
 }
 
-void removeMovie()
+void removeMovie(struct Movie **head)
 {
+    char movieName[20];
+    printf("\nEnter name of the movie to be removed: ");
+    scanf(" ");
+    fgets(movieName, 20, stdin);
+    movieName[strcspn(movieName, "\n")] = 0;
+    struct Movie *temp, *previous = NULL;
+    temp = *head;
+    if (temp == NULL)
+    {
+        printf("\nNo Movies has been added for the day");
+        return;
+    }
+    else
+    {
+        while (temp != NULL)
+        {
+            printf("\nINPUT : %s", movieName);
+            printf("\nDATA : %s", temp->name);
+            if (strcmp(movieName, temp->name) == 0)
+            {
+                printf("innnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn");
+                if (temp->screen1 == 1)
+                {
+                    screen1 = NULL;
+                }
+                if (temp->screen2 == 1)
+                {
+                    screen2 = NULL;
+                }
+                if (temp->screen3 == 1)
+                {
+                    screen3 = NULL;
+                }
+                if (previous == NULL && temp->next == NULL)
+                {
+                    movieHead = NULL;
+                }
+                else if (previous == NULL)
+                {
+                    *head = temp->next;
+                }
+                else
+                {
+                    previous->next = temp->next;
+                }
+                free(temp);
+                break;
+            }
+            previous = temp;
+            temp = temp->next;
+        }
+    }
 }
 
 void displaySeats()
@@ -263,22 +432,36 @@ void admin()
         printf("\n1. Add Movie");
         printf("\n2. Remove Movie");
         printf("\n3. Remove Show");
-        printf("\n4. Exit");
+        printf("\n4. Display Movies");
+        printf("\n5. Display Shows");
+        printf("\n6. Exit");
         printf("\nEnter your option: ");
         scanf("%d", &option);
         switch (option)
         {
         case 1:
-            setMovie(&movieHead);
+            if (screen1 == NULL || screen2 == NULL || screen3 == NULL)
+            {
+                setMovie(&movieHead, &screen1, &screen2, &screen3);
+            }
+            else
+            {
+                printf("\nNo Screens Available");
+            }
             break;
         case 2:
-            removeMovie();
+            removeMovie(&movieHead);
             break;
         case 3:
-            showMovies(&movieHead);
             // remove show()
             break;
         case 4:
+            showMovies(&movieHead);
+            break;
+        case 5:
+            displayShows(&screen1, &screen2, &screen3);
+            break;
+        case 6:
             return;
         default:
             break;
@@ -296,7 +479,7 @@ void user()
         printf("2. Book Seat\n");
         printf("3. Cancel Seat\n");
         printf("4. Go to previous Menu\n");
-        scanf("%d",&choice);
+        scanf("%d ", &choice);
         switch (choice)
         {
         case 1:
