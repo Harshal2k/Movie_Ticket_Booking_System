9aa2b223d88e206fa9f62b4f48dc327d0cad8abf
Harshal Gosawi <harshalgosawi@gmail.com>
2022-12-28 21:02:54 +0530

    Fixed bugs

--- ticketBookingSystem.c
+++ ticketBookingSystem.c
@@ -78,7 +78,15 @@ int isDateValid(int dd, int mm, int yy)
         {
             // check days
             if ((dd >= 1 && dd <= 31) && (mm == 1 || mm == 3 || mm == 5 || mm == 7 || mm == 8 || mm == 10 || mm == 12))
-                return 1 else if ((dd >= 1 && dd <= 30) && (mm == 4 || mm == 6 || mm == 9 || mm == 11)) return 1 else if ((dd >= 1 && dd <= 28) && (mm == 2)) return 1 else if (dd == 29 && mm == 2 && (yy % 400 == 0 || (yy % 4 == 0 && yy % 100 != 0))) return 1 else printf("Day is invalid.\n");
+                return 1;
+            else if ((dd >= 1 && dd <= 30) && (mm == 4 || mm == 6 || mm == 9 || mm == 11))
+                return 1;
+            else if ((dd >= 1 && dd <= 28) && (mm == 2))
+                return 1;
+            else if (dd == 29 && mm == 2 && (yy % 400 == 0 || (yy % 4 == 0 && yy % 100 != 0)))
+                return 1;
+            else
+                printf("Day is invalid.\n");
             return 0;
         }
         else
